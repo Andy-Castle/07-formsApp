70a5453d65fbcf92aa22fca5fe8bef17d7ae9ae1
Andy Castle <masterchief_and@live.com>
2025-01-29 11:00:33 -0500

    Validators: Como hacer las validaciones

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -9,21 +14,24 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 export class BasicPageComponent {
   // Este es una forma de hacer formularios o los campos
   // public myForm: FormGroup = new FormGroup({
-  //   name: new FormControl(''),
+  //   name: new FormControl('', []),
   //   price: new FormControl(0),
   //   inStorage: new FormControl(0),
   // });
 
   //Esta es otra forma
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    // El Validators es para hacer validaciones
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
   constructor(private fb: FormBuilder) {}
 
   onSave(): void {
+    if (this.myForm.invalid) return;
+
     console.log(this.myForm.value);
   }
 }
