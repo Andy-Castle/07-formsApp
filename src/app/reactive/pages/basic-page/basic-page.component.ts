9a656b4296703ce75e7374569af975d0f195ed54
Andy Castle <masterchief_and@live.com>
2025-01-29 10:46:12 -0500

    1er step Formularios Reactivos: Aqui se vio como crear las variables de nuestro formulario en TS y como conectarlo a nuestro html

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicPageComponent {
+  // Este es una forma de hacer formularios o los campos
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
 
+  //Esta es otra forma
+  public myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 }
