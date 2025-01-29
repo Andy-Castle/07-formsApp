bd15e1193da38e4d51a8112fd132fed13c385000
Andy Castle <masterchief_and@live.com>
2025-01-29 15:27:09 -0500

    Elaborar el validador del correo: en esta sección se vio como hacer el validador asincrono, osea que le pide al backend el correo para vreificar si es que existe ya un correo asi, si existe lanzara la advertencia

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -4,16 +4,43 @@ import {
   AsyncValidator,
   ValidationErrors,
 } from '@angular/forms';
-import { delay, Observable, of } from 'rxjs';
+import { delay, map, Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class EmailValidator implements AsyncValidator {
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
-    console.log({ email });
 
-    return of({
-      emailTaken: true,
-    }).pipe(delay(2000));
+    const httpCallObservable = new Observable<ValidationErrors | null>(
+      (subscriber) => {
+        console.log({ email });
+
+        if (email === 'andy@gmail.com') {
+          subscriber.next({ emailTaken: true });
+          subscriber.complete();
+          // return;
+        }
+
+        subscriber.next(null);
+        subscriber.complete();
+      }
+    ).pipe(delay(300));
+
+    return httpCallObservable;
   }
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log({ email });
+
+  //   return of({
+  //     emailTaken: true,
+  //   }).pipe(delay(2000));
+  // }
+
+  // return this.http.get<any[]>(`https://localhost:3000/users?q=${email}`).pipe(
+  //   map((resp) => {
+  //     return resp.length === 0 ? null : { emailTaken: true };
+  //   })
+  // );
 }
