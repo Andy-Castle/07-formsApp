2c41b2c319c0d5a84d68c391936a3f52fa377581
Andy Castle <masterchief_and@live.com>
2025-01-29 11:55:17 -0500

    Formulario dinámicos: Se esta viendo las primeras partes de los formularios dinamicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DynamicPageComponent {
+  // public myForm2 = new FormGroup({
+  //   favoriteGames: new FormArray([]),
+  // });
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Mario Bros', Validators.required],
+    ]),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  get favoriteGames() {
+    return this.myForm.controls['favoriteGames'] as FormArray;
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset();
+  }
 }
