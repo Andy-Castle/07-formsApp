b61c62db77f2e359be798800fd811e3b43b295f8
Andy Castle <masterchief_and@live.com>
2025-01-29 11:20:56 -0500

    1er Opción para mostrar errores de validacion en la pantalla, este no es mi favorito, requiere mucho codigo en el html

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -36,7 +36,10 @@ export class BasicPageComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   onSave(): void {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
 
     console.log(this.myForm.value);
 
@@ -47,6 +50,6 @@ export class BasicPageComponent implements OnInit {
   ngOnInit(): void {
     //Esto es cuando recien se carga el form, este carga los datos
     //de la variable, osea esto podria venir del backend
-    this.myForm.reset(rtx5090);
+    // this.myForm.reset(rtx5090);
   }
 }
