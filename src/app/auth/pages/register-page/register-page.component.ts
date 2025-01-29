bb125c92af6420be20c76f4e34370c2c37c4cd36
Andy Castle <masterchief_and@live.com>
2025-01-29 13:40:02 -0500

    Se implemento la funcionalidad del formulario en el register-page.component

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -6,4 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './register-page.component.html',
   styles: [],
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string) {
+    // TODO: obtener validación desde un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
